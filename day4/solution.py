466cd419ec31fd4f7d49f81d98308cf3373e6c78
sleepyfoxen <git@foxen.uk>
2024-12-04 07:12:03 +0000

    sleepy creature hours i go bed now :zzz:

--- day4/solution.py
+++ day4/solution.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 with open('input', 'r') as f:
     data = f.read().strip().splitlines()
@@ -8,16 +9,17 @@ directions = ((0, 1), (0, -1), (1, 0), (-1, 0),
 def add(t1, t2):
     return tuple(map(sum, zip(t1, t2)))
 
-def at(row, col, data=data):
-    if (row, col) != clamp(row, col): return ''
-    else: row, col = clamp(row, col)
-    return data[row][col]
+def mul(t, n):
+    return reduce(add, [t] * n)
 
 def clamp(row, col, data=data):
-    row_hi = len(data) - 1
-    col_hi = len(data[0]) - 1
-
+    row_hi, col_hi = len(data) - 1, len(data[0]) - 1
     return (max(min(row, row_hi), 0), max(min(col, col_hi), 0))
+    
+def at(row, col, data=data):
+    if (row, col) != clamp(row, col): return ''
+    return data[row][col]
+
 
 count = 0
 for direction in directions:
@@ -25,8 +27,8 @@ for direction in directions:
         for col in range(len(data[row])):
             x = at(row, col)
             m = at(*add((row, col), direction))
-            a = at(*add((row, col), add(direction, direction)))
-            s = at(*add((row, col), add(direction, add(direction, direction))))
+            a = at(*add((row, col), mul(direction, 2)))
+            s = at(*add((row, col), mul(direction, 3)))
 
             xmas = ''.join((x, m, a, s))
             if xmas == 'XMAS':
@@ -42,12 +44,11 @@ for row in range(len(data)):
         a = at(row, col)
         m1 = at(*add((row, col), directions[0][0]))
         s1 = at(*add((row, col), directions[0][1]))
-        m2 = at(*add((row, col), directions[1][1]))
-        s2 = at(*add((row, col), directions[1][0]))
+        m2 = at(*add((row, col), directions[1][0]))
+        s2 = at(*add((row, col), directions[1][1]))
 
         mas1 = ''.join((m1, a, s1))
         mas2 = ''.join((m2, a, s2))
-
         if mas1 in ('MAS', 'SAM') and mas2 in ('MAS', 'SAM'):
             count += 1
 
